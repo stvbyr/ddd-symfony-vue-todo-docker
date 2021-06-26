ce5dd8befec8490ee374d19334a1d463a4d13080
sentou-sy <beyer_steve@outlook.de>
2021-06-26 18:03:54 +0200

    edit: update migrations and ignore custom tables

--- symfony/migrations/Version20210626153725.php
+++ symfony/migrations/Version20210626153725.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210528085724 extends AbstractMigration
+final class Version20210626153725 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,20 @@ final class Version20210528085724 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $table = $schema->createTable('app_todo');
+        $table = $schema->createTable('app_habit');
 
         $table->addColumn('uuid', 'string');
         $table->addColumn('title', 'string');
-        $table->addColumn('scheduled_date', 'date');
-        $table->addColumn('status', 'string');
-        $table->addColumn('user', 'integer');
+        $table->addColumn('from', 'date');
+        $table->addColumn('to', 'date');
+        $table->addColumn('frequency', 'string');
+        $table->addColumn('user', 'string');
+        $table->addColumn('moves', 'json');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $schema->dropTable('app_todo');
+        $schema->dropTable('app_habit');
     }
 }
