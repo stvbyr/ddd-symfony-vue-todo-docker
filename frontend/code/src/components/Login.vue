2daf53bcf8e9add5f99d18b22deb96f02ff9cf72
Steve <beyer_steve@outlook.de>
2021-12-12 10:27:45 +0100

    Dev (#10)
    * add phpmyadmin
    
    * persistent storage for mysql data
    
    check in dbdata folder for automatic creation
    helps with dealing with permission issues
    
    * use same permission strategy for nginx data
    
    * install vue-router
    
    * update jwt config
    
    * add function to get all todo items
    
    * begin with frontend
    
    * add tsconfig
    
    * vue add typescript
    
    * typescript config
    
    * composer update
    
    * install nelmio cors
    
    * frontend

--- frontend/code/src/components/Login.vue
+++ frontend/code/src/components/Login.vue
@@ -1,11 +1,43 @@
 <template>
     <div class="hello">
         <h1>Login</h1>
+        <form>
+            <div>
+                <label for="username">Username</label
+                ><input v-model="username" />
+            </div>
+            <div>
+                <label for="password">Password></label
+                ><input type="password" v-model="password" />
+            </div>
+            <div>
+                <button type="button" @click="login">Login</button>
+            </div>
+        </form>
     </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import loginApi from './../api/Login';
+
+const username = ref('');
+const password = ref('');
+
+const login = async () => {
+    const result = await loginApi.fetchToken({
+        username: username.value,
+        password: password.value,
+    });
+    console.log(result);
+};
+
+export default defineComponent({
+    props: {},
+    setup() {
+        return { username, password, login };
+    },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- frontend/code/src/components/Todos.vue
+++ frontend/code/src/components/Todos.vue
@@ -1,11 +1,29 @@
 <template>
     <div class="hello">
-        <h1>Habits</h1>
+        <h1>Todos</h1>
+        <router-link :to="{ name: 'todo.create' }">🆕 New Todo </router-link>
+        <ul>
+            <li v-for="todo in todos" :key="todo.uuid">{todo.title}</li>
+        </ul>
     </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent } from 'vue';
+import todosApi from './../api/Todos';
+
+const todos = async () => {
+    return await todosApi.fetchAllTodos();
+};
+
+export default defineComponent({
+    props: {},
+    setup(props) {
+        return {
+            todos,
+        };
+    },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
