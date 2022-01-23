1fc01743dfbad9889d763cca9cf21b54ec43b873
Steve <beyer_steve@outlook.de>
2022-01-23 14:00:13 +0100

    initial vite install

--- frontend/code/component.d.ts
+++
@@ -1,9 +0,0 @@
-// components.d.ts
-declare module '@vue/runtime-core' {
-    export interface GlobalComponents {
-        RouterLink: typeof import('vue-router')['RouterLink'];
-        RouterView: typeof import('vue-router')['RouterView'];
-    }
-}
-
-export {};
